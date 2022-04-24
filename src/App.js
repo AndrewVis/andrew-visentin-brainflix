9b7e854ad0a585a325ae198de5264f54a5220adf
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-04-24 11:25:30 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.scss';
 import Header from './components/Header/Header.js';
 import { Component } from 'react';
-import Home from './Pages/Home/Home';
-import Upload from './Pages/Upload/Upload';
+import Home from './page/Home/Home.js';
+import Upload from './page/Upload/Upload.js';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 
 class App extends Component {
