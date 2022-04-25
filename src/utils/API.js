6dd5ce965d32f9ce6842807597239a5e7017a24f
AndrewVis <97456285+AndrewVis@users.noreply.github.com>
2022-04-25 17:52:23 -0400

    api key fixed

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,6 +5,6 @@ export const API_KEY = "f3958c42-1d3f-41f4-b887-c7c5b39c8c19"
 
 export const API_CALLS = {
 
-  getVideos: () => axios.get(`${API_URL}/videos/${API_KEY}`),
-  getDetailedVideos: (id) => axios.get(`${API_URL}/videos/${id}${API_KEY}`),
+  getVideos: () => axios.get(`${API_URL}/videos?api_key=${API_KEY}`),
+  getDetailedVideos: (id) => axios.get(`${API_URL}/videos/${id}?api_key=${API_KEY}`),
 }
